456a24166deb48ee60ff3d74d39114eb4083f909
wout <schoovaertswout@live.be>
2019-04-28 18:11:48 +0200

    Updating checkstyle

--- src/test/java/com/github/zwaldeck/modern/jcr/StreamBinaryTest.java
+++ src/test/java/com/github/zwaldeck/modern/jcr/StreamBinaryTest.java
@@ -33,7 +33,7 @@ class StreamBinaryTest {
     }
 
     @Test
-    void getStream_success() throws Exception{
+    void getStream_success() throws Exception {
         InputStream inputStream = mock(InputStream.class);
         when(binaryMock.getStream()).thenReturn(inputStream);
 
@@ -41,35 +41,35 @@ class StreamBinaryTest {
     }
 
     @Test
-    void getStream_exception() throws Exception{
+    void getStream_exception() throws Exception {
         when(binaryMock.getStream()).thenThrow(RepositoryException.class);
 
         assertThrows(ModernJcrException.class, () -> binary.getStream());
     }
 
     @Test
-    void read_success() throws Exception{
+    void read_success() throws Exception {
         when(binaryMock.read(any(byte[].class), anyLong())).thenReturn(9);
 
-        assertEquals(9, binary.read(new byte[] {}, 15));
+        assertEquals(9, binary.read(new byte[]{}, 15));
     }
 
     @Test
-    void read_exception() throws Exception{
+    void read_exception() throws Exception {
         when(binaryMock.read(any(byte[].class), anyLong())).thenThrow(RepositoryException.class);
 
-        assertThrows(ModernJcrException.class, () -> binary.read(new byte[] {}, 15));
+        assertThrows(ModernJcrException.class, () -> binary.read(new byte[]{}, 15));
     }
 
     @Test
-    void getSize_success() throws Exception{
+    void getSize_success() throws Exception {
         when(binaryMock.getSize()).thenReturn(9L);
 
         assertEquals(9L, binary.getSize());
     }
 
     @Test
-    void getSize_exception() throws Exception{
+    void getSize_exception() throws Exception {
         when(binaryMock.getSize()).thenThrow(RepositoryException.class);
 
         assertThrows(ModernJcrException.class, () -> binary.getSize());
